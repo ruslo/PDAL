81ed4a9eaece79f6a9f83f104eae9ded6a34763a
Ruslan Baratov <x@ruslo.dev>
2022-05-16 15:27:25 +0800

    Fix dependencies

--- cmake/geotiff.cmake
+++ cmake/geotiff.cmake
@@ -2,9 +2,12 @@
 # GeoTIFF support
 #
 
-find_package(GeoTIFF REQUIRED 1.3.0)
+find_package(GeoTIFF CONFIG REQUIRED 1.3.0)
 set_package_properties(GeoTIFF PROPERTIES TYPE REQUIRED)
 if (GEOTIFF_FOUND)
+    find_package(PROJ4 CONFIG REQUIRED)
+    set(GEOTIFF_INCLUDE_DIR ${GeoTIFF_INCLUDE_DIRS} CACHE PATH "")
+    set(GEOTIFF_LIBRARY "${GeoTIFF_LIBRARIES};PROJ4::proj" CACHE PATH "")
     include_directories("${GEOTIFF_INCLUDE_DIR}")
     set(PDAL_HAVE_LIBGEOTIFF 1)
     mark_as_advanced(CLEAR TIFF_INCLUDE_DIR)
